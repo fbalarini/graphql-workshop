fa9dbe7f854471710352c849411def65d0811a4a
Federico Balarini <federico@xmartlabs.com>
2020-06-03 17:42:57 -0300

    Queries

--- graphql-api/app/graphql/types/query_type.rb
+++ graphql-api/app/graphql/types/query_type.rb
@@ -1,13 +1,10 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :all_blogs, [BlogType], null: false, description: 'Return all the blogs'
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # This method is invoked, when `all_blogs` fields is being resolved
+    def all_blogs
+      Blog.all
     end
   end
 end
